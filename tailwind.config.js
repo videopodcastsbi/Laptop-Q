5652a440ccf500b86930c731162365089980ca7a
videopodcastsbi <suhat020855@gmail.com>
2025-01-23 14:22:19 +0700

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./*.html"],
+  theme: {
+    extend: {
+      backgroundImage: {
+        "headers":"url('../img/laptopstopicpage-2048px-3685-2x1-1.jpg')"
+      }
+    },
+  },
+  plugins: [],
+}
+
